800186f2f105bc4964a9a70d4fbd563c13fd4d77
Mohammad Hemayet Ullah <40654463+hemayet-nirjhoy@users.noreply.github.com>
2022-09-15 15:05:18 +0600

    basic todo store is setup including the json placeholder and simple view

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="text-align:center">
+    <h2>Todo Manager Application</h2>
+  </div>
+  <div class="container">
+    <TodoItems />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItems from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoItems,
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container{
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
